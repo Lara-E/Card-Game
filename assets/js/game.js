3e474aa9c8f777e369fcc84d4d8abb889c9b499f
Lara Eller <lareh_eller@yahoo.com>
2019-08-06 14:18:17 -0400

    styling cards

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,6 +20,7 @@ function getDeck()
 
 function shuffle()
 {
+	console.log("shuffling")
 	// for 1000 turns
 	// switch the values of two random cards
 	for (var i = 0; i < 1000; i++)
@@ -31,6 +32,7 @@ function shuffle()
 		deck[location1] = deck[location2];
 		deck[location2] = tmp;
 	}
+	renderDeck();
 }
 
 function renderDeck()
@@ -42,22 +44,32 @@ function renderDeck()
 		var card = document.createElement("div");
 		var value = document.createElement("div");
 		var suit = document.createElement("div");
+		var suitFlip = document.createElement("div");
 		var bottom = document.createElement("div");
 		card.className = "card";
 		value.className = "value";
 		suit.className = "suit " + deck[i].Suit;
-		bottom.className = "bottom"
+		suitFlip.className = deck[i].Suit + "-flip flip";
+		bottom.className = "bottom";
 
 		value.innerHTML = deck[i].Value;
 		bottom.innerHTML = deck[i].Value;
 		card.appendChild(value);
 		card.appendChild(suit);
+		card.appendChild(suitFlip);
 		card.appendChild(bottom);
 
 		document.getElementById("deck").appendChild(card);
 	}
 }
 
+
 getDeck();
 shuffle();
 renderDeck();
+document.getElementById("shuffle").addEventListener("click", function(event) {
+	console.log("clicked")
+	event.preventDefault();
+	shuffle();
+});
+
