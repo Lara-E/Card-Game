68640d8fb117199971fb003ce985dd7b7c941927
Lara Eller <lareh_eller@yahoo.com>
2019-08-09 10:11:10 -0400

    layout for dealing

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,15 +2,12 @@ var deck = new Array();
 var suits = ["spades", "diamonds", "clubs", "hearts"];
 var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-function getDeck()
-{
+function getDeck() {
 	deck = new Array();
 
-	for(var i = 0; i < suits.length; i++)
-	{
-		for(var x = 0; x < values.length; x++)
-		{
-			var card = {Value: values[x], Suit: suits[i]};
+	for (var i = 0; i < suits.length; i++) {
+		for (var x = 0; x < values.length; x++) {
+			var card = { Value: values[x], Suit: suits[i] };
 			deck.push(card);
 		}
 	}
@@ -18,13 +15,11 @@ function getDeck()
 	return deck;
 }
 
-function shuffle()
-{
+function shuffle() {
 	console.log("shuffling")
 	// for 1000 turns
 	// switch the values of two random cards
-	for (var i = 0; i < 1000; i++)
-	{
+	for (var i = 0; i < 1000; i++) {
 		var location1 = Math.floor((Math.random() * deck.length));
 		var location2 = Math.floor((Math.random() * deck.length));
 		var tmp = deck[location1];
@@ -32,44 +27,63 @@ function shuffle()
 		deck[location1] = deck[location2];
 		deck[location2] = tmp;
 	}
-	renderDeck();
+	// renderDeck();
 }
 
-function renderDeck()
-{
-      document.getElementById("deck").innerHTML = "";
+function renderDeck() {
+	document.getElementById("stock").innerHTML = "";
 
-	for(var i = 0; i < deck.length; i++)
-	{
+	for (var i = 0; i < deck.length; i++) {
 		var card = document.createElement("div");
 		var value = document.createElement("div");
-		var suit = document.createElement("div");
-		var suitFlip = document.createElement("div");
 		var bottom = document.createElement("div");
 		card.className = "card";
+		card.setAttribute("data-suit", deck[i].Suit);
+		card.setAttribute("data-value", deck[i].Value);
+		card.setAttribute("data-face", "up");
+		card.setAttribute("data-holder", "down");
 		value.className = "value";
-		suit.className = "suit " + deck[i].Suit;
-		suitFlip.className = deck[i].Suit + "-flip flip";
 		bottom.className = "bottom";
 
 		value.innerHTML = deck[i].Value;
 		bottom.innerHTML = deck[i].Value;
 		card.appendChild(value);
-		card.appendChild(suit);
-		card.appendChild(suitFlip);
 		card.appendChild(bottom);
 
-		document.getElementById("deck").appendChild(card);
+		document.getElementById("stock").appendChild(card);
+
+		setClick();
 	}
 }
 
 
 getDeck();
 shuffle();
-renderDeck();
+// renderDeck();
 document.getElementById("shuffle").addEventListener("click", function(event) {
 	console.log("clicked")
 	event.preventDefault();
 	shuffle();
 });
 
+
+function logData() {
+	var dataFace = this.getAttribute("data-face");
+	var dataHolder = this.getAttribute("data-holder")
+	this.setAttribute("data-face", dataHolder);
+	this.setAttribute("data-holder", dataFace)
+	
+	
+}
+function setClick() {
+	var cards = document.getElementsByClassName("card");
+	// console.log(cards)
+
+	for (var i = 0; i < cards.length; i++) {
+		cards[i].addEventListener("click", logData, false)
+		// console.log(cards[i])
+	}
+}
+
+
+  
